56f0e7fcdc08592c883e11cbcecbd87e58cf9571
Askar Gafurov <japdlsd@gmail.com>
2014-11-05 19:58:03 +0100

    Added hlupy client. (mega hlupy).

---
+++ klienti/hlupy/main.cpp
@@ -0,0 +1,50 @@
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <unistd.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#include "common.h"
+#include "marshal.h"
+#include "update.h"
+
+
+Mapa mapa;
+Stav stav;   // vzdy som hrac cislo 0
+
+
+// main() zavola tuto funkciu, ked nacita mapu
+void inicializuj() {
+  // (sem patri vas kod)
+}
+
+
+// main() zavola tuto funkciu, ked chce vediet, ake prikazy chceme vykonat,
+// co tato funkcia rozhodne pomocou: prikazy.push_back(Prikaz(...));
+Prikaz zistiTah() {
+  // (sem patri vas kod)
+  return Prikaz(rand() % 4, true);
+}
+
+
+int main() {
+  // v tejto funkcii su vseobecne veci, nemusite ju menit (ale mozte).
+
+  unsigned int seed = time(NULL) * getpid();
+  srand(seed);
+
+  nacitaj(cin, mapa);
+  fprintf(stderr, "START pid=%d, seed=%u\n", getpid(), seed);
+  inicializuj();
+
+  while (cin.good()) {
+    nacitaj(cin, stav);
+    uloz(cout, zistiTah());
+    cout << ".\n" << flush;   // bodka a flush = koniec odpovede
+  }
+
+  return 0;
+}
+
